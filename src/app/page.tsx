454a641a3ad7f7c04c08aa0325bcf862c53f9935
Anthony Tleiji <anthonytleiji@gmail.com>
2024-01-05 00:38:22 +0200

    Add posts schema Add trpc posts getAll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
+import { api } from "~/trpc/server";
 
 export default async function Home() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const data = await api.posts.getAll.query();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <UserButton afterSignOutUrl="/" />
+
+      <h1 className="text-3xl font-bold">Posts</h1>
+      <div>{data?.map((post) => <div>{post.content}</div>)}</div>
     </main>
   );
 }
