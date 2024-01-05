95148cbe311efdabe1a3b436db61ed3c7cf3d105
Anthony Tleiji <anthonytleiji@gmail.com>
2024-01-05 18:21:21 +0200

    Add posts Fetch users with post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,67 @@
-import { UserButton, auth } from "@clerk/nextjs";
-import { api } from "~/trpc/server";
+"use client";
 
-export default async function Home() {
-  const { userId } = auth();
+import { useState } from "react";
+import { api } from "~/trpc/react";
+import { UserButton, useUser } from "@clerk/nextjs";
 
-  if (!userId) {
+const CreatePostWizard = () => {
+  const { user } = useUser();
+
+  const [input, setInput] = useState("");
+
+  if (!user) {
     return null;
   }
 
-  const data = await api.posts.getAll.query();
+  return (
+    <div className="flex w-full gap-3">
+      <UserButton
+        afterSignOutUrl="/"
+        appearance={{
+          elements: {
+            userButtonAvatarBox: {
+              width: 56,
+              height: 56,
+            },
+          },
+        }}
+      />
+      <input
+        placeholder="Type some emojis!"
+        className="grow bg-transparent outline-none"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={false}
+      />
+    </div>
+  );
+};
+
+export default function Home() {
+  const getPosts = api.posts.getAll.useQuery();
+
+  if (getPosts.error) {
+    return <div>Error: {getPosts.error.message}</div>;
+  }
+  if (getPosts.isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <UserButton afterSignOutUrl="/" />
-
-      <h1 className="text-3xl font-bold">Posts</h1>
-      <div>{data?.map((post) => <div>{post.content}</div>)}</div>
+    <main className="flex h-screen justify-center">
+      <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
+        <div className="flex border-b border-slate-400 p-4">
+          <CreatePostWizard />
+        </div>
+        <div className="flex flex-col">
+          {getPosts.data?.map(({ post, author }) => (
+            <div key={post.id} className="border-b border-slate-400 p-8">
+              {post.content}
+            </div>
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
