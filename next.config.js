4f77ea0c115f9b18e799cc4127e046040c58a0ee
Anthony Tleiji <anthonytleiji@gmail.com>
2024-01-07 19:37:50 +0200

    Fix PWA

--- next.config.js
+++ next.config.js
@@ -4,11 +4,12 @@
  */
 await import("./src/env.js");
 
+import withPWA from "next-pwa";
+
 /** @type {import("next").NextConfig} */
-const withPWA = require("next-pwa");
 const config = {
-  // @ts-ignore
   ...withPWA({
+    // @ts-ignore
     pwa: {
       dest: "public",
       register: true,
