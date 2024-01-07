76c0a6a3961de3f3393ffd6dc5fc2cf84b9ce65b
Anthony Tleiji <anthonytleiji@gmail.com>
2024-01-07 18:50:55 +0200

    Add Assets Add logo to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { auth } from "@clerk/nextjs";
+import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+
+import Logo from "~/assets/logo.png";
 
 export default function Home() {
   const { userId } = auth();
@@ -9,6 +12,13 @@ export default function Home() {
   }
   return (
     <div className="flex h-full w-full flex-col items-center justify-center gap-3">
+      <Image
+        className="mb-5"
+        src={Logo}
+        width={150}
+        height={150}
+        alt="SecondBrain Logo"
+      />
       <h1 className="text-xl">Welcome to twitter emoji only clone!</h1>
       <button className="border border-slate-400 p-1">
         <Link href="/feed">Get Started</Link>
