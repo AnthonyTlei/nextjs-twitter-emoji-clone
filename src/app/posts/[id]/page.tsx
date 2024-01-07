4ba01d31e628d10e21bc96dfa7a5f012bfe77f2d
Anthony Tleiji <anthonytleiji@gmail.com>
2024-01-07 16:51:45 +0200

    Add Post methods - getById - getPostsByUserId
    Add addUSerDataToPosts helper
    
    Add posts single page template

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { cache } from "react";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { api } from "~/trpc/server";
 
-export const metadata = {
-  title: "Post - Twitter Emoji Clone",
-};
+interface SinglePostPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const getPostWithAuthorById = cache(async (id: string) => {
+  try {
+    const postWithAuthor = await api.posts.getById.query({
+      id,
+    });
+    return postWithAuthor;
+  } catch (error) {
+    return null;
+  }
+});
+
+export async function generateMetadata({
+  params: { id },
+}: SinglePostPageProps): Promise<Metadata> {
+  const postWithAuthor = await getPostWithAuthorById(id);
+  if (!postWithAuthor) {
+    notFound();
+  }
+  return {
+    title: postWithAuthor.author.username + " 's Post - Twitter Emoji Clone",
+    description: "Post by " + postWithAuthor.author.username,
+    openGraph: {
+      images: [{ url: postWithAuthor.author.profileImageUrl }],
+    },
+  };
+}
 
 const SinglePostPage = () => {
   return <div>Single Post Page</div>;
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -9,6 +9,7 @@ import {
   privateProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
+import { addUserDataToPosts } from "~/server/helpers/addUserDataToPost";
 import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 const ratelimit = new Ratelimit({
@@ -18,37 +19,44 @@ const ratelimit = new Ratelimit({
 });
 
 export const postsRouter = createTRPCRouter({
+  getById: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const post = await ctx.db.post.findUnique({
+        where: { id: input.id },
+      });
+
+      if (!post) throw new TRPCError({ code: "NOT_FOUND" });
+
+      return (await addUserDataToPosts([post]))[0];
+    }),
+
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.db.post.findMany({
       take: 100,
-      orderBy: {
-        createdAt: "desc",
-      },
+      orderBy: [{ createdAt: "desc" }],
     });
 
-    const users = (
-      await clerkClient.users.getUserList({
-        userId: posts.map((post) => post.authorId),
-        limit: 100,
-      })
-    ).map(filterUserForClient);
-
-    return posts.map((post) => {
-      const author = users.find((user) => user.id === post.authorId);
-
-      if (!author) {
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Author not found",
-        });
-      }
-
-      return {
-        post,
-        author,
-      };
-    });
+    return addUserDataToPosts(posts);
   }),
+
+  getPostsByUserId: publicProcedure
+    .input(
+      z.object({
+        userId: z.string(),
+      }),
+    )
+    .query(({ ctx, input }) =>
+      ctx.db.post
+        .findMany({
+          where: {
+            authorId: input.userId,
+          },
+          take: 100,
+          orderBy: [{ createdAt: "desc" }],
+        })
+        .then(addUserDataToPosts),
+    ),
 
   create: privateProcedure
     .input(
